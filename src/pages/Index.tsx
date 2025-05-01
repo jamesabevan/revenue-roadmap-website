f9f12dcf5172327ba6984ed0e8d999bdcff3ef00
jamesabevan <james.bevan17@gmail.com>
2025-05-01 13:28:03 +0100

    Update Index.tsx - for consultation button

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-
 import Layout from "@/components/layout/Layout";
 import HeroSection from "@/components/home/HeroSection";
 import ServicesSection from "@/components/home/ServicesSection";
@@ -6,14 +5,28 @@ import ExpertiseSection from "@/components/home/ExpertiseSection";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import ContactSection from "@/components/home/ContactSection";
 import CtaSection from "@/components/home/CtaSection";
+import { ScheduleConsultationButton } from "@/components/ScheduleConsultationButton";
 
 const Index = () => {
   return (
     <Layout>
       <HeroSection />
+      <div className="flex justify-center my-6">
+        <ScheduleConsultationButton />
+      </div>
+
       <ServicesSection />
+      <div className="flex justify-center my-6">
+        <ScheduleConsultationButton />
+      </div>
+
       <ExpertiseSection />
       <TestimonialsSection />
+
+      <div className="flex justify-center my-6">
+        <ScheduleConsultationButton />
+      </div>
+
       <ContactSection />
       <CtaSection />
     </Layout>
