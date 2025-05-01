1b21e13e0a902acde2cf2ebe798234c7e49d3868
jamesabevan <james.bevan17@gmail.com>
2025-05-01 13:32:29 +0100

    Update Index.tsx - revert back

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,28 +5,14 @@ import ExpertiseSection from "@/components/home/ExpertiseSection";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import ContactSection from "@/components/home/ContactSection";
 import CtaSection from "@/components/home/CtaSection";
-import { ScheduleConsultationButton } from "@/components/ScheduleConsultationButton";
 
 const Index = () => {
   return (
     <Layout>
       <HeroSection />
-      <div className="flex justify-center my-6">
-        <ScheduleConsultationButton />
-      </div>
-
       <ServicesSection />
-      <div className="flex justify-center my-6">
-        <ScheduleConsultationButton />
-      </div>
-
       <ExpertiseSection />
       <TestimonialsSection />
-
-      <div className="flex justify-center my-6">
-        <ScheduleConsultationButton />
-      </div>
-
       <ContactSection />
       <CtaSection />
     </Layout>
